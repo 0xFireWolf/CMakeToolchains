5c766a962d399b18118f4ed89e26901df283334d
FireWolf <austere.j@gmail.com>
2022-05-11 22:39:06 -0700

    Clang 13: Adjust the path on macOS (x86-64).

--- x86-64_Clang-13_macOS_Homebrew.cmake
+++ x86-64_Clang-13_macOS_Homebrew.cmake
@@ -1,7 +1,7 @@
 #
 # Toolchain: x86-64 with Clang 13
-# Installed: From Homebrew (brew install llvm)
+# Installed: From Homebrew (brew install llvm@13)
 # Host Mach: macOS
 #
-set(CMAKE_C_COMPILER                /usr/local/opt/llvm/bin/clang)
-set(CMAKE_CXX_COMPILER              /usr/local/opt/llvm/bin/clang++)
+set(CMAKE_C_COMPILER                /usr/local/opt/llvm@13/bin/clang)
+set(CMAKE_CXX_COMPILER              /usr/local/opt/llvm@13/bin/clang++)

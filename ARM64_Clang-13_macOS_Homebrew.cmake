7ad16181ebae4c79c5ed6667dc4d4b82dff1d6fe
FireWolf <austere.j@gmail.com>
2022-05-13 12:50:12 -0700

    Add toolchains for Apple Silicon-based Mac.

---
+++ ARM64_Clang-13_macOS_Homebrew.cmake
@@ -0,0 +1,7 @@
+#
+# Toolchain: ARM64 with Clang 13 (Apple Silicon)
+# Installed: From Homebrew (brew install llvm@13)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /opt/homebrew/opt/llvm@13/bin/clang)
+set(CMAKE_CXX_COMPILER              /opt/homebrew/opt/llvm@13/bin/clang++)
---
+++ ARM64_GCC-11_macOS_Homebrew.cmake
@@ -0,0 +1,7 @@
+#
+# Toolchain: ARM64 with GCC 11 (Apple Silicon)
+# Installed: From Homebrew (brew install gcc@11)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /opt/homebrew/bin/gcc-11)
+set(CMAKE_CXX_COMPILER              /opt/homebrew/bin/g++-11)

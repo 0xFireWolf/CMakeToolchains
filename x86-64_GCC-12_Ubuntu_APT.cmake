2aec3ddedcdc190a6422b5fed44986b2c45580ac
FireWolf <austere.j@gmail.com>
2022-05-12 18:47:52 -0700

    Add GCC 12 and Clang 14 toolchains on Ubuntu.

---
+++ x86-64_GCC-12_Ubuntu_APT.cmake
@@ -0,0 +1,7 @@
+#
+# Toolchain: x86-64 with GCC 12
+# Installed: From APT
+# Host Mach: Ubuntu
+#
+set(CMAKE_C_COMPILER                /usr/bin/gcc-12)
+set(CMAKE_CXX_COMPILER              /usr/bin/g++-12)

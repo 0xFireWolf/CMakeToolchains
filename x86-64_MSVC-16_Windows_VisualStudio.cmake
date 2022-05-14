2b0de0b9148a2bf4af7fc169d52ef1c8bab8d956
FireWolf <austere.j@gmail.com>
2022-05-14 15:07:56 -0700

    Add MSVC 2019 and 2022 toolchains on Windows.

---
+++ x86-64_MSVC-16_Windows_VisualStudio.cmake
@@ -0,0 +1,8 @@
+#
+# Toolchain: x86-64 with MSVC 16.x
+# Installed: From Visual Studio 2019
+# Host Mach: Windows
+#     Usage: Developer Powershell/Command Prompt for VS 2019
+#
+set(CMAKE_C_COMPILER                cl.exe)
+set(CMAKE_CXX_COMPILER              cl.exe)
---
+++ x86-64_MSVC-17_Windows_VisualStudio.cmake
@@ -0,0 +1,8 @@
+#
+# Toolchain: x86-64 with MSVC 17.x
+# Installed: From Visual Studio 2022
+# Host Mach: Windows
+#     Usage: Developer Powershell/Command Prompt for VS 2022
+#
+set(CMAKE_C_COMPILER                cl.exe)
+set(CMAKE_CXX_COMPILER              cl.exe)

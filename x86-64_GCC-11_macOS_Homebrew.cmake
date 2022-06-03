4e77516968c3aee4327a09edb1804d4fcbd71d3b
FireWolf <austere.j@gmail.com>
2022-06-02 23:28:05 -0700

    Added GCC 12 for macOS.

--- x86-64_GCC-11_macOS_Homebrew.cmake
+++ x86-64_GCC-11_macOS_Homebrew.cmake
@@ -1,6 +1,6 @@
 #
 # Toolchain: x86-64 with GCC 11
-# Installed: From Homebrew (brew install gcc)
+# Installed: From Homebrew (brew install gcc@11)
 # Host Mach: macOS
 #
 set(CMAKE_C_COMPILER                /usr/local/bin/gcc-11)
---
+++ x86-64_GCC-12_macOS_Homebrew.cmake
@@ -0,0 +1,7 @@
+#
+# Toolchain: x86-64 with GCC 12
+# Installed: From Homebrew (brew install gcc@12)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /usr/local/bin/gcc-12)
+set(CMAKE_CXX_COMPILER              /usr/local/bin/g++-12)

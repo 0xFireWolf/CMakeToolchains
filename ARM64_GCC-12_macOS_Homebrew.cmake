b196f69eedbd97bc6e32c7332ce6726621eccedf
FireWolf <austere.j@gmail.com>
2022-06-08 15:52:25 -0700

    Added GCC 12 for Apple Sillicon-based Macs.

---
+++ ARM64_GCC-12_macOS_Homebrew.cmake
@@ -0,0 +1,7 @@
+#
+# Toolchain: ARM64 with GCC 12 (Apple Silicon)
+# Installed: From Homebrew (brew install gcc@12)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /opt/homebrew/bin/gcc-12)
+set(CMAKE_CXX_COMPILER              /opt/homebrew/bin/g++-12)

b2ecd1b9aae48130a367011a64738c5f6f08d71b
FireWolf <austere.j@gmail.com>
2022-06-22 00:27:34 -0700

    Added AppleClang 14 for macOS.

---
+++ ARM64_AppleClang-14_macOS_Xcode.cmake
@@ -0,0 +1,7 @@
+#
+# Toolchain: ARM64 with Apple Clang (Apple Silicon)
+# Installed: Xcode / Command Line Developer Tools
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /usr/bin/clang)
+set(CMAKE_CXX_COMPILER              /usr/bin/clang++)

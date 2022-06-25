0e81ebbd0938245afeec0eda728febd005d1c60c
FireWolf <austere.j@gmail.com>
2022-06-24 20:42:05 -0700

    Added Clang 14 for macOS.

---
+++ x86-64_Clang-14_macOS_Homebrew.cmake
@@ -0,0 +1,7 @@
+#
+# Toolchain: x86-64 with Clang 14
+# Installed: From Homebrew (brew install llvm@14)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /usr/local/opt/llvm@14/bin/clang)
+set(CMAKE_CXX_COMPILER              /usr/local/opt/llvm@14/bin/clang++)

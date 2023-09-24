94d5a046cf8035e26b9913f1ef521badb00de1e4
FireWolf <austere.j@gmail.com>
2023-09-23 17:50:11 -0700

    Add Clang 17 and Apple Clang 15 toolchains.

---
+++ x86-64_Clang-17_macOS_Homebrew.cmake
@@ -0,0 +1,19 @@
+#
+# Toolchain: x86-64 with Clang 17
+# Installed: From Homebrew (brew install llvm@17)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /usr/local/opt/llvm@17/bin/clang)
+set(CMAKE_CXX_COMPILER              /usr/local/opt/llvm@17/bin/clang++)
+set(LLVM_PROFDATA                   /usr/local/opt/llvm@17/bin/llvm-profdata)
+set(LLVM_COV                        /usr/local/opt/llvm@17/bin/llvm-cov)
+
+# Chainload Toolchain File Support
+if (DEFINED CHAOS_CHAINLOAD_TOOLCHAIN_FILE)
+    if (EXISTS ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+        message(STATUS "Including additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE}...")
+        include(${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+    else()
+        message(WARNING "Specified additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE} does not exist.")
+    endif()
+endif()

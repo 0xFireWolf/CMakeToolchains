963dcff9974e40e7b89b0d745f56a538a41dec15
FireWolf <austere.j@gmail.com>
2024-10-31 17:51:05 -0700

    Add support for AppleClang 16 on macOS.

---
+++ x86-64_AppleClang-16_macOS_Xcode.cmake
@@ -0,0 +1,17 @@
+#
+# Toolchain: x86-64 with Apple Clang
+# Installed: Xcode / Command Line Developer Tools
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /usr/bin/clang)
+set(CMAKE_CXX_COMPILER              /usr/bin/clang++)
+
+# Chainload Toolchain File Support
+if (DEFINED CHAOS_CHAINLOAD_TOOLCHAIN_FILE)
+    if (EXISTS ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+        message(STATUS "Including additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE}...")
+        include(${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+    else()
+        message(WARNING "Specified additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE} does not exist.")
+    endif()
+endif()

2f0bbd7c66ece45b8d8c35eee9219aa01fe181f5
FireWolf <austere.j@gmail.com>
2025-10-26 07:40:56 -0700

    Add support for Clang 21.

---
+++ ARM64_Clang-21_macOS_Homebrew.cmake
@@ -0,0 +1,22 @@
+#
+# Toolchain: ARM64 with Clang 21 (Apple Silicon)
+# Installed: From Homebrew (brew install llvm@21)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /opt/homebrew/opt/llvm@21/bin/clang)
+set(CMAKE_CXX_COMPILER              /opt/homebrew/opt/llvm@21/bin/clang++)
+
+# Chainload Toolchain File Support
+if (DEFINED CHAOS_CHAINLOAD_TOOLCHAIN_FILE)
+    if (EXISTS ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+        message(STATUS "Including additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE}...")
+        include(${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+    else()
+        message(WARNING "Specified additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE} does not exist.")
+    endif()
+endif()
+
+if (CHAOS_USE_BUNDLED_LIBCPP)
+    message(STATUS "Updating the linker flags for executables to use the bundled libc++.")
+    set(CMAKE_EXE_LINKER_FLAGS "-L/opt/homebrew/opt/llvm@21/lib/c++ -L/opt/homebrew/opt/llvm@21/lib/unwind -lunwind")
+endif()
---
+++ x86-64_Clang-21_Ubuntu_APT.cmake
@@ -0,0 +1,22 @@
+#
+# Toolchain: x86-64 with Clang 21
+# Installed: From APT
+# Host Mach: Ubuntu
+#
+set(CMAKE_C_COMPILER                /usr/bin/clang-21)
+set(CMAKE_CXX_COMPILER              /usr/bin/clang++-21)
+
+set(CMAKE_CXX_FLAGS                 "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+
+set(LLVM_PROFDATA                   /usr/bin/llvm-profdata-21)
+set(LLVM_COV                        /usr/bin/llvm-cov-21)
+
+# Chainload Toolchain File Support
+if (DEFINED CHAOS_CHAINLOAD_TOOLCHAIN_FILE)
+    if (EXISTS ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+        message(STATUS "Including additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE}...")
+        include(${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+    else()
+        message(WARNING "Specified additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE} does not exist.")
+    endif()
+endif()

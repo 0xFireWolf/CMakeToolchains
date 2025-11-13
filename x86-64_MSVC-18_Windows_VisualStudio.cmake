43b8ceb4585975ab56483ac72cdf82e4854275d1
FireWolf <austere.j@gmail.com>
2025-11-13 12:45:49 -0800

    Add support for Visual Studio 2026.

---
+++ x86-64_MSVC-18_Windows_VisualStudio.cmake
@@ -0,0 +1,19 @@
+#
+# Toolchain: x86-64 with MSVC 18.x
+# Installed: From Visual Studio 2026
+# Host Mach: Windows
+#     Usage: Developer Powershell/Command Prompt for VS 2026
+# Note that CMAKE_C_COMPILER and CMAKE_CXX_COMPILER are computed automatically from the toolset variable.
+# 
+set(CMAKE_GENERATOR_TOOLSET "v145")
+set(CMAKE_GENERATOR_PLATFORM "x64")
+
+# Chainload Toolchain File Support
+if (DEFINED CHAOS_CHAINLOAD_TOOLCHAIN_FILE)
+    if (EXISTS ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+        message(STATUS "Including additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE}...")
+        include(${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+    else()
+        message(WARNING "Specified additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE} does not exist.")
+    endif()
+endif()

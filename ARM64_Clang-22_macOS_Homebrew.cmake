9ef6c0df586d5028eb50bf816f4dfe9325e4b822
FireWolf <austere.j@gmail.com>
2026-03-02 10:03:58 +0800

    Add support for Clang 22.

---
+++ ARM64_Clang-22_macOS_Homebrew.cmake
@@ -0,0 +1,22 @@
+#
+# Toolchain: ARM64 with Clang 22 (Apple Silicon)
+# Installed: From Homebrew (brew install llvm@22)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /opt/homebrew/opt/llvm@22/bin/clang)
+set(CMAKE_CXX_COMPILER              /opt/homebrew/opt/llvm@22/bin/clang++)
+
+# Chainload Toolchain File Support
+if (DEFINED CHAOS_CHAINLOAD_TOOLCHAIN_FILE)
+    if (EXISTS ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+        message(STATUS "Including additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE}...")
+        include(${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+    else()
+        message(WARNING "Specified additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE} does not exist.")
+    endif()
+endif()
+
+if (CHAOS_USE_BUNDLED_LIBCPP)
+    message(STATUS "Updating the linker flags for executables to use the bundled libc++.")
+    set(CMAKE_EXE_LINKER_FLAGS "-L/opt/homebrew/opt/llvm@22/lib/c++ -L/opt/homebrew/opt/llvm@22/lib/unwind -lunwind")
+endif()
---
+++ x86-64_Clang-22_macOS_Homebrew.cmake
@@ -0,0 +1,24 @@
+#
+# Toolchain: x86-64 with Clang 22
+# Installed: From Homebrew (brew install llvm@22)
+# Host Mach: macOS
+#
+set(CMAKE_C_COMPILER                /usr/local/opt/llvm@22/bin/clang)
+set(CMAKE_CXX_COMPILER              /usr/local/opt/llvm@22/bin/clang++)
+set(LLVM_PROFDATA                   /usr/local/opt/llvm@22/bin/llvm-profdata)
+set(LLVM_COV                        /usr/local/opt/llvm@22/bin/llvm-cov)
+
+# Chainload Toolchain File Support
+if (DEFINED CHAOS_CHAINLOAD_TOOLCHAIN_FILE)
+    if (EXISTS ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+        message(STATUS "Including additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE}...")
+        include(${CHAOS_CHAINLOAD_TOOLCHAIN_FILE})
+    else()
+        message(WARNING "Specified additional CMake toolchain file ${CHAOS_CHAINLOAD_TOOLCHAIN_FILE} does not exist.")
+    endif()
+endif()
+
+if (CHAOS_USE_BUNDLED_LIBCPP)
+    message(STATUS "Updating the linker flags for executables to use the bundled libc++.")
+    set(CMAKE_EXE_LINKER_FLAGS "-L/usr/local/opt/llvm@22/lib/c++ -L/usr/local/opt/llvm@22/lib/unwind -lunwind")
+endif()
